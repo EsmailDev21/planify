80f81b782ffd9dd770731b8a286279f94ea9a24f
esmail <smail.anonymous8@gmail.com>
2024-08-30 00:48:03 +0000

    logo fix

--- components/logo/PlanifyLogo.tsx
+++ components/logo/PlanifyLogo.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import planifyLogo from "../../public/assets/icons/planify-logo.png";
 import Image from "next/image";
+
 const PlanifyLogo = () => {
   return (
-    <div className="flex flex-row justify-center items-center">
-      <Image alt="quanta logo" src={planifyLogo} width={40} height={40} />
-      <div className="mx-2">
-        <p className="font-semibold text-primary text-base">Planify</p>
-      </div>
+    <div className="flex items-center space-x-2 hover:scale-105 transition-transform duration-300 ease-in-out">
+      <Image
+        alt="Planify logo"
+        src={planifyLogo}
+        width={40} // Increased size for better visibility
+        height={40} // Increased size for better visibility
+        className="rounded-full" // Added border and rounded effect
+      />
+      <p className="font-bold text-primary text-lg sm:text-xl md:text-2xl transition-colors duration-300 ease-in-out hover:text-secondary">
+        Planify
+      </p>
     </div>
   );
 };
