a0d706d5d47a0a19c0ac32804ba8bb6821a4562a
EsmailDev21 <esmailkhorchani.dev@gmail.com>
2024-11-23 19:25:18 +0100

    more quotes

--- lib/mock/quotes.mock.ts
+++ lib/mock/quotes.mock.ts
@@ -351,6 +351,256 @@ const quotes: QuoteModel[] = [
       },
     ],
   },
+  {
+    id: "quote4",
+    projectId: "project4",
+    sender: autoEntrepreneur,
+    client: client,
+    amount: 7000,
+    description:
+      "Devis pour le développement d'une application mobile pour la gestion des finances personnelles.",
+    status: QuoteStatus.PENDING,
+    createdAt: new Date("2024-10-01"),
+    updatedAt: new Date("2024-10-02"),
+    title: "Développement Application Mobile",
+    dueDate: new Date("2024-11-15"),
+    thumbnail: "/assets/images/project_thumbnails/project4.jpg",
+    address: "789 Tech Valley, Villeurbanne, État, 12345",
+    items: [
+      {
+        id: "item1",
+        type: QuoteItemType.TASK,
+        unitPrice: 3000,
+        quantity: 1,
+        tva: 10,
+        totalPrice: 3300,
+        task: {
+          id: "task41",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 14),
+          priority: Priority.HIGH,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Sam Wright",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Design UX/UI",
+          description:
+            "Création de wireframes et prototypes interactifs pour l'application.",
+        },
+      },
+      {
+        id: "item2",
+        type: QuoteItemType.TASK,
+        unitPrice: 4000,
+        quantity: 1,
+        tva: 10,
+        totalPrice: 4400,
+        task: {
+          id: "task42",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 28),
+          priority: Priority.HIGH,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Sophia Lee",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Développement Frontend et Backend",
+          description:
+            "Construction des interfaces utilisateur et des fonctionnalités backend pour l'application.",
+        },
+      },
+    ],
+  },
+  {
+    id: "quote5",
+    projectId: "project5",
+    sender: businessOwner,
+    client: client,
+    amount: 20000,
+    description:
+      "Devis pour la réalisation d'une campagne marketing numérique incluant SEO, gestion des médias sociaux et publicités payantes.",
+    status: QuoteStatus.ACCEPTED,
+    createdAt: new Date("2024-09-10"),
+    updatedAt: new Date("2024-09-12"),
+    title: "Campagne Marketing Numérique",
+    dueDate: new Date("2024-11-01"),
+    thumbnail: "/assets/images/project_thumbnails/project5.jpg",
+    address: "101 Digital Hub, Villeurbanne, État, 54321",
+    items: [
+      {
+        id: "item1",
+        type: QuoteItemType.TASK,
+        unitPrice: 7000,
+        quantity: 1,
+        tva: 15,
+        totalPrice: 8050,
+        task: {
+          id: "task51",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 10),
+          priority: Priority.MEDIUM,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Emma Brown",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Optimisation SEO",
+          description:
+            "Améliorer la visibilité du site web grâce à des pratiques SEO avancées.",
+        },
+      },
+      {
+        id: "item2",
+        type: QuoteItemType.TASK,
+        unitPrice: 10000,
+        quantity: 1,
+        tva: 15,
+        totalPrice: 11500,
+        task: {
+          id: "task52",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 30),
+          priority: Priority.HIGH,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Liam Wilson",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Gestion des Médias Sociaux",
+          description:
+            "Création et gestion de contenu pour les comptes sociaux du client.",
+        },
+      },
+      {
+        id: "item3",
+        type: QuoteItemType.TASK,
+        unitPrice: 3000,
+        quantity: 1,
+        tva: 15,
+        totalPrice: 3450,
+        task: {
+          id: "task53",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 15),
+          priority: Priority.MEDIUM,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Olivia Davis",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Publicité en Ligne",
+          description:
+            "Configuration et gestion des campagnes Google Ads et Facebook Ads.",
+        },
+      },
+    ],
+  },
+  {
+    id: "quote6",
+    projectId: "project6",
+    sender: autoEntrepreneur,
+    client: client,
+    amount: 15000,
+    description:
+      "Devis pour l'organisation d'un événement professionnel, y compris la gestion logistique, la coordination et la conception visuelle.",
+    status: QuoteStatus.PENDING,
+    createdAt: new Date("2024-09-25"),
+    updatedAt: new Date("2024-09-27"),
+    title: "Organisation Événementiel",
+    dueDate: new Date("2024-12-01"),
+    thumbnail: "/assets/images/project_thumbnails/project6.avif",
+    address: "333 Event Plaza, Villeurbanne, État, 67890",
+    items: [
+      {
+        id: "item1",
+        type: QuoteItemType.TASK,
+        unitPrice: 5000,
+        quantity: 1,
+        tva: 20,
+        totalPrice: 6000,
+        task: {
+          id: "task61",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 20),
+          priority: Priority.HIGH,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Ethan White",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Planification Logistique",
+          description:
+            "Réservation des lieux, gestion des prestataires et planification du programme.",
+        },
+      },
+      {
+        id: "item2",
+        type: QuoteItemType.TASK,
+        unitPrice: 8000,
+        quantity: 1,
+        tva: 20,
+        totalPrice: 9600,
+        task: {
+          id: "task62",
+          color: generateRandomColor(),
+          endDate: addDays(new Date(), 25),
+          priority: Priority.HIGH,
+          progress: 0,
+          startDate: new Date(),
+          status: Status.TODO,
+          tags: [],
+          teamMembers: [
+            {
+              fullName: "Mia Clark",
+              profilePhoto:
+                "/assets/images/avatars/" + generateRandomAvatar() + ".png",
+            },
+          ],
+          title: "Coordination de l'Événement",
+          description:
+            "Superviser l'installation, coordonner les équipes et assurer le bon déroulement de l'événement.",
+        },
+      },
+    ],
+  },
 ];
 
 export { quotes as sampleQuotes };
